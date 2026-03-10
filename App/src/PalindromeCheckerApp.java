9e9d63088c8e41b968ea56111e3fdf9e910a0d04
VIJAI ADITHYA <vixai0719@gamil.com>
2026-03-10 15:16:18 +0530

    Vijai Added UC2 Hardcoded Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,16 +1,19 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String str = "madam";
-        String rev = "";
 
-        for(int i=str.length()-1;i>=0;i--){
-            rev = rev + str.charAt(i);
+        System.out.println("Welcome to Palindrome Checker App");
+
+        String word = "madam";
+        String reverse = "";
+
+        for(int i = word.length() - 1; i >= 0; i--){
+            reverse += word.charAt(i);
         }
 
-        if(str.equals(rev)){
-            System.out.println("Palindrome");
+        if(word.equals(reverse)){
+            System.out.println(word + " is a Palindrome");
         } else {
-            System.out.println("Not Palindrome");
+            System.out.println(word + " is not a Palindrome");
         }
     }
 }
