de9154bcf09d4f7153c754863b616d1f974fa8a2
VIJAI ADITHYA <vixai0719@gamil.com>
2026-03-10 15:40:40 +0530

    Vijai Added UC9 Recursive Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,27 +1,21 @@
-import java.util.*;
-
 public class PalindromeCheckerApp {
+
+    static boolean isPalindrome(String word) {
+
+        if(word.length() <= 1)
+            return true;
+
+        if(word.charAt(0) != word.charAt(word.length()-1))
+            return false;
+
+        return isPalindrome(word.substring(1, word.length()-1));
+    }
 
     public static void main(String[] args) {
 
         String word = "madam";
 
-        LinkedList<Character> list = new LinkedList<>();
-
-        for(char c : word.toCharArray()) {
-            list.add(c);
-        }
-
-        boolean isPalindrome = true;
-
-        while(list.size() > 1) {
-            if(list.removeFirst() != list.removeLast()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if(isPalindrome)
+        if(isPalindrome(word))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
