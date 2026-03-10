56f71269405b82a41659e1fedf48ba012ffb99cd
VIJAI ADITHYA <vixai0719@gamil.com>
2026-03-10 15:22:46 +0530

    Vijai Added UC3 User Input Palindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,16 +1,23 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Welcome to Palindrome Checker App");
 
-        String word = "madam";
+        System.out.print("Enter a word: ");
+        String word = sc.nextLine();
+
         String reverse = "";
 
-        for(int i = word.length() - 1; i >= 0; i--){
+        for(int i = word.length() - 1; i >= 0; i--) {
             reverse += word.charAt(i);
         }
 
-        if(word.equals(reverse)){
+        if(word.equals(reverse)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is not a Palindrome");
